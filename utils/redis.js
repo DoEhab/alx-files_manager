90c2526b4b12013ab2a586bde7c19e666c1bbde5
dohaehab <dohaehab@gmail.com>
2025-06-25 21:30:11 +0300

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,49 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+
+    this.client.connect()
+      .then(() => console.log('Connected to Redis'))
+      .catch((err) => console.error('Redis connection failed:', err));
+  }
+
+  isAlive() {
+    return this.client.isOpen;
+  }
+
+  async get(key) {
+    try {
+      return await this.client.get(key);
+    } catch (err) {
+      console.error('Redis GET Error:', err);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, {
+        EX: duration,
+      });
+    } catch (err) {
+      console.error('Redis SET Error:', err);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error('Redis DEL Error:', err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
